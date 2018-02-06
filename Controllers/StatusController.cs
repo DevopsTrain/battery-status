e9e20090d8fe1692a2429c0f47ded4318182471b
Tobias Eichner <tobias.eichner@partner.bmw.de>
2018-02-06 16:27:55 +0100

    add controller for liveness and readiness probes

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BatteryStatus.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        [Route("live")]
+        public IActionResult GetLive()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("readiness")]
+        public IActionResult GetReadiness()
+        {
+            return Ok();
+        }
+    }
+}
