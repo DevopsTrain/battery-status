2f32b2a63245604a03de7a4f14bab02705288336
Felix Zinkel <felix.zinkel@gmail.com>
2018-02-20 16:10:51 +0100

    Added config for azure k8

--- deploy/k8s/envsubst.sh
+++ deploy/k8s/envsubst.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # call with build number (image tag for ci built docker image) as single argument
 sed -e "s/\${BUILD_NUMBER}/$1/" deploy-CI.template.yaml  > deploy-CI.yaml
+sed -e "s/\${BUILD_NUMBER}/$1/" deploy-CI-Azure.template.yaml  > deploy-CI-Azure.yaml
 nl deploy-CI.yaml
